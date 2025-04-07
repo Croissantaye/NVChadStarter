9ec628ec0672135d8efdc5b836f83ef9c68950fe
croissantaye <asa81099@gmail.com>
2025-04-06 22:10:18 -0700

    add python support

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map( {"i", "n"}, "jk", "<ESC>")
+map("t", "jk", "<C-\\><C-N>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>")
