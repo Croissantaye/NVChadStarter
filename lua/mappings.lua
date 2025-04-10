7c9c2db9c667383b40aa1e1cda5b38161ca40311
Azevedo, Asante <AAzevedo@lnw.com>
2025-04-10 16:11:55 -0700

    add gproxy keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,10 +9,16 @@ map( {"i", "n"}, "jk", "<ESC>")
 map("t", "jk", "<C-\\><C-N>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-map("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>")
-map("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>")
+map("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Telescope go to definition" })
+map("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>", { desc = "Telescopte find references" })
 
-map("n", "<leader>te", "<cmd>tabe<CR>")
-map("n", "<leader>tn", "<cmd>tabn<CR>")
-map("n", "<leader>tp", "<cmd>tabp<CR>")
-map("n", "<leader>tx", "<cmd>tabc<CR>")
+map("n", "<leader>te", "<cmd>tabe<CR>", { desc = "new tab" })
+map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "next tab" })
+map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "previous tab" })
+map("n", "<leader>tx", "<cmd>tabc<CR>", { desc = "close tab" })
+
+-- map for auto running gdm-gproxy
+map( "n" , "<leader>gp", function()
+    require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+    vim.fn.feedkeys("gulp --gdm-gproxy\r", "i")
+end, { desc = "terminal toggle floating term" })
